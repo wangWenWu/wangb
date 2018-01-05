ccc2700df1e4a14c9bb2bb7bb7bd5c0ba5372c01
WangBin <849850352@qq.com>
2018-01-05 11:48:46 +0800

    bug fix

---
+++ js/createMenu.js
@@ -0,0 +1,13 @@
+/**
+ * -------------------------------------
+ * 自动生成导航菜单
+ * -------------------------------------
+ * WangBin.2018.01.05.snow
+*/
+var menuLiHtml = '<li><a href="index.html"><span>首页</span></a></li>';
+menuLiHtml += '<li><a href="timebar.html"><span>时间轴</span></a></li>';
+/*menuLiHtml += '<li><a href=""><span>旅行</span></a></li>';*/
+/*menuLiHtml += '<li><a href=""><span>相册</span></a></li>';*/
+menuLiHtml += '<li><a href="contact.html"><span>我的家</span></a></li>';
+
+$(".menu_box_list ul,.dropdown-menu").html(menuLiHtml);
