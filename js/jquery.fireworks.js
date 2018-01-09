294adbbf772a7b5c9130e48f427b8c5cef6cbf1b
wangwenwu <849850352@qq.com>
2018-01-09 09:47:10 +0800

    0109

--- js/jquery.fireworks.js
+++ js/jquery.fireworks.js
@@ -5,7 +5,7 @@
 
     options.sound = options.sound || false;
 		options.opacity = options.opacity || 1;
-		options.width = '1900';//options.width || $(this).width();
+		options.width = options.width || $(this).width();
 		options.height = options.height || $(this).height();
 
     var fireworksField = this,
