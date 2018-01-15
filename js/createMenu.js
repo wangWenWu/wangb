16a35740cf5cf5e32a2d9e9756eec4e0feb36083
wangwenwu <849850352@qq.com>
2018-01-15 10:46:46 +0800

    0115

--- js/createMenu.js
+++ js/createMenu.js
@@ -6,8 +6,8 @@
 */
 var menuLiHtml = '<li><a href="index.html"><span>首页</span></a></li>';
 menuLiHtml += '<li><a href="timebar.html"><span>时间轴</span></a></li>';
-/*menuLiHtml += '<li><a href=""><span>旅行</span></a></li>';*/
+menuLiHtml += '<li><a href="travel.html"><span>旅行</span></a></li>';
 /*menuLiHtml += '<li><a href=""><span>相册</span></a></li>';*/
-menuLiHtml += '<li><a href="contact.html"><span>我的家</span></a></li>';
+menuLiHtml += '<li><a href="contact.html"><span>我们的家</span></a></li>';
 
 $(".menu_box_list ul,.dropdown-menu").html(menuLiHtml);
