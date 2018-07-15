69ec6d19c3b0e597289e8a141b4975a8076a71ce
doc.Wang <849850352@qq.com>
2018-07-15 10:22:44 +0800

    Update initTimeBarHtml.js

--- js/initTimeBarHtml.js
+++ js/initTimeBarHtml.js
@@ -106,5 +106,11 @@ function DoPageS()
 					
 function initCntr()
 {
-	setupElements();
+	$('.cntl').cntl({
+		revealbefore: 300,
+		anim_class: 'cntl-animate',
+		onreveal: function(e){
+			//console.log(e);
+		}
+	});
 }
